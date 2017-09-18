3bb1395fdf851793eba031c8f3f489544957b0a6
John Masamalo <31832566+jmasamalo@users.noreply.github.com>
2017-09-17 22:52:05 -0500

    commit1

---
+++ Homework10/hw10_question1.ts
@@ -0,0 +1,19 @@
+﻿type BankAccountType = {money:number,deposit(value:number):void};
+
+let bankAccount:BankAccountType = {
+    money: 2000,
+    deposit(value): void {
+        this.money += value;
+    }
+};
+
+type myselfType = {name:string,bankAccount:BankAccountType,hobbies:string[]};
+
+let myself:myselfType = {
+    name: "Asaad",
+    bankAccount: bankAccount,
+    hobbies: ["Violin","Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+console.log(myself);
