38c0abde637c57bfffe45914708cecd632dd29e1
John Masamalo <31832566+jmasamalo@users.noreply.github.com>
2017-09-17 23:38:41 -0500

    commit2

---
+++ Homework10/hw10_question2.ts
@@ -0,0 +1,20 @@
+﻿class Car{
+    constructor(public name:string, public acceleration:number =0) {
+        this.name = name;
+        this.acceleration = acceleration;
+    }
+
+    honk():void{
+        console.log(`${this.name} is saying: Tooooooooot!`);
+    }
+
+    accelerate(speed:number):void{
+        this.acceleration += speed;
+    }
+}
+
+var car:Car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(60);
+console.log(car.acceleration);
