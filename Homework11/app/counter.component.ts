dd277dcc077bb1c08ab73321289b53c53719002d
John Masamalo <31832566+jmasamalo@users.noreply.github.com>
2017-09-19 23:02:07 -0500

    commit

---
+++ Homework11/app/counter.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `<button (click)="dec()">-</button>{{counterValue}}<button (click)="inc()">+</button>`,
+  styles: ['button{background-color:#bbf; border:1; font-size:17px}']
+})
+export class CounterComponent implements OnInit {
+
+    @Input() counterValue = 0;
+
+    @Output() counterChange = new EventEmitter();
+
+    constructor() { }
+
+    dec() {
+        this.counterValue -= 1;
+        this.counterChange.emit(this.counterValue);
+    }
+
+    inc() {
+        this.counterValue += 1;
+        this.counterChange.emit(this.counterValue);
+    }
+
+  ngOnInit() {
+  }
+
+}
